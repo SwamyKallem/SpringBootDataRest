7632fb4fbc83caadfa6fce7affbe08b4dd39edb3
swamy <kyswamy9@gmail.com>
2024-03-03 00:51:24 -0500

    'springrest'

--- src/main/java/com/hibernatedirty/configuration/CORSFilter.java
+++ src/main/java/com/hibernatedirty/configuration/CORSFilter.java
@@ -1,6 +1,11 @@
 package com.hibernatedirty.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
@@ -12,29 +17,23 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@Component
-public class CORSFilter implements Filter {
+@Configuration
+public class CORSFilter {
 
+	@Bean
+	public WebMvcConfigurer corsMappingConfigurer() {
+	   return new WebMvcConfigurer() {
+	       @Override
+	       public void addCorsMappings(CorsRegistry registry) {
+	           registry.addMapping("/**")
+	             .allowedOrigins("*")
+	             .allowedMethods("GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD")
+	             .maxAge(3600)
+	             .allowedHeaders("Requestor-Type")
+	             .exposedHeaders("X-Get-Header");
+	       }
+	   };
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-        response.setHeader("Access-Control-Expose-Headers", "Location");
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-
+}
 }
 
